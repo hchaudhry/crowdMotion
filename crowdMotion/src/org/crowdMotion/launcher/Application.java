1dca11d4a3e4135eb337baa0e13b3c261a68dc04
hussam <chaudhry.hussam@gmail.com>
2014-11-07 21:40:24 +0100

    Class MapRender: * Displaying a window

--- crowdMotion/src/org/crowdMotion/launcher/Application.java
+++ crowdMotion/src/org/crowdMotion/launcher/Application.java
@@ -1,14 +1,33 @@
 package org.crowdMotion.launcher;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 import org.crowdMotion.models.Field;
 import org.crowdMotion.utils.MapReader;
+import org.crowdMotion.utils.MapRender;
 
 public class Application {
 
-	
 	public static void main(String[] args) {
 		Field f = new Field("/home/hussam/Bureau/Foule/map.txt");
 		String s = MapReader.readMapFile(f.getMapFileName());
 		System.out.println(s);
+
+		createWindow(new MapRender());
+	}
+
+	static public void createWindow(MapRender render) {
+		JFrame frame = new JFrame("FrameDemo");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setPreferredSize(new Dimension(1050, 480));
+		frame.setContentPane(render);
+
+		frame.pack();
+
+		frame.setVisible(true);
 	}
 }
--- crowdMotion/src/org/crowdMotion/utils/MapRender.java
+++ crowdMotion/src/org/crowdMotion/utils/MapRender.java
@@ -1,5 +1,42 @@
 package org.crowdMotion.utils;
 
-public class MapRender {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class MapRender extends JPanel{
+
+	JPanel panelTop;
+	JPanel panelMainBottomContainer;
+	JPanel panelBottomLeft;
+	JPanel panelBottomRight;
+
+	public MapRender() {
+		super();
+
+		this.setLayout(new BorderLayout());
+		
+		panelTop = new JPanel();
+		panelMainBottomContainer = new JPanel();
+		panelBottomLeft = new JPanel();
+		panelBottomRight = new JPanel();
+		
+		panelBottomLeft.setPreferredSize(new Dimension(this.getWidth() / 2, 100));
+		
+		panelMainBottomContainer.add(panelBottomLeft, BorderLayout.WEST);
+		panelMainBottomContainer.add(panelBottomRight, BorderLayout.EAST);
+		
+		panelTop.setBackground(Color.blue);
+		panelBottomLeft.setBackground(Color.green);
+		panelBottomRight.setBackground(Color.RED);
+		
+		
+		this.add(panelTop, BorderLayout.CENTER);
+		this.add(panelMainBottomContainer, BorderLayout.SOUTH);
+		
+		
+	}
 
 }
